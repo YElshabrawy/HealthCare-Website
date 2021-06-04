8c6236d5ee7b174deee6bc53c33fe3e9248b2586
Youssef Elshabrawy <YIElshabrawy@gmail.com>
2021-06-04 18:53:07 +0200

    Revert "Helmy_Tpush"
    This reverts commit 619409468df0356c65abe429ceac957bef46d288.

--- PHPF/Patient/ShowPatientProfile.php
+++ PHPF/Patient/ShowPatientProfile.php
@@ -2,38 +2,4 @@
 
 include_once '../connectDataBase.php';
 
-session_start();
-$ID = $_SESSION['SignInID'];
-
-$_SESSION['PFName'];
-$_SESSION['PLName'];
-$_SESSION['Pemail'];
-$_SESSION['PGender'];
-$_SESSION['PCountry'];
-$_SESSION['PWeight'];
-$_SESSION['PBirthdate'];
-$_SESSION['PCity'];
-$_SESSION['PHeight'];
-$_SESSION['PPhoneNumber'];
-$_SESSION['PAllergies'];
-$_SESSION['PChronicDiseases'];
-$_SESSION['PSurgerys'];
-$_SESSION['PMedicine'];
-
 $result = ShowPatientProfile($ID);
-if($result)
-    {
-
-        while($row = mysqli_fetch_assoc($result)){
-            $UserType = $row['AccountType'];
-            echo $UserType;
-        } 
-    }
-    else{
-        echo "Error";
-    } 
-
-$result2 = patientFHistory($ID);
-$result3 = patientHistory($ID);
-
-
